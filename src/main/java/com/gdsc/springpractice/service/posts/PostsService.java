3b1c507419de66236776ffd0218634d98f4e05b4
jiy00nn <wldbs204@gmail.com>
2022-05-06 17:48:42 +0900

    chapter3: add posts update and read function

--- src/main/java/com/gdsc/springpractice/service/posts/PostsService.java
+++ src/main/java/com/gdsc/springpractice/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.gdsc.springpractice.service.posts;
 
+import com.gdsc.springpractice.domain.posts.Posts;
 import com.gdsc.springpractice.domain.posts.PostsRepository;
+import com.gdsc.springpractice.web.dto.PostsResponseDto;
 import com.gdsc.springpractice.web.dto.PostsSaveRequestDto;
+import com.gdsc.springpractice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("There's no post. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("There's no post. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/gdsc/springpractice/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.gdsc.springpractice.web.dto;
+
+import com.gdsc.springpractice.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
