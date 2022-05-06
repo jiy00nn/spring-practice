b50fa3371a839a3873dc167715491b938d3f7c1c
jiy00nn <wldbs204@gmail.com>
2022-05-06 16:38:20 +0900

    chapter2: add entity and repository about posts

---
+++ src/main/java/com/gdsc/springpractice/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.gdsc.springpractice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
