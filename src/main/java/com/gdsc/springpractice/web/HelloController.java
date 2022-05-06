32c4d9126aa78dd3bc01052b36e5453b65f5cafd
jiy00nn <wldbs204@gmail.com>
2022-05-06 16:37:11 +0900

    chapter2: add test code

---
+++ src/main/java/com/gdsc/springpractice/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.gdsc.springpractice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
