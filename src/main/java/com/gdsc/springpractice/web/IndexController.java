e54eb9c9ad18a0b39fb529538ae368ff3430f383
jiy00nn <wldbs204@gmail.com>
2022-05-24 14:40:53 +0900

    chapter4: add Initial view

--- src/main/java/com/gdsc/springpractice/web/IndexController.java
+++ src/main/java/com/gdsc/springpractice/web/IndexController.java
@@ -1,2 +1,13 @@
-package com.gdsc.springpractice.web;public class IndexController {
+package com.gdsc.springpractice.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
